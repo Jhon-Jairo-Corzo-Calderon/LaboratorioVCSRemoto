564b1fdc7b146433a88d1a8c6d52aac65c83c47c
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-03-30 13:58:57 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,5 @@
+a=int(input("Digite un número:\n"))
+c=int(input("Digite otro número:\n"))
+
+print("El doble del primer número ingresado es:\n",a*2)
+print("El producto de los números ingresados es:\n",a*c)
