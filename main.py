69d29b83fec7138db32e31173c707e2ed9994064
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-03-30 16:38:56 -0500

    Mi primera modificacion

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-a=int(input("Digite un número:\n"))
-c=int(input("Digite otro número:\n"))
+a=float(input("Digite un número:\n"))
+b=int(input("Digite un número entero:\n"))
+c=float(input("Digite otro número:\n"))
+d=int(input("Digite un número positivo:\n"))
 
 print("El doble del primer número ingresado es:\n",a*2)
 print("El producto de los números ingresados es:\n",a*c)
+print("El cuadrado del número",b,"es:\n",b*b,)
+print("La raíz del número",d,"es:\n",d**1/2,)
+
