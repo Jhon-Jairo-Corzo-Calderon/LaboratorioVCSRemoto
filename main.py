72244dbd3556ee220485c52e6a447245f5f290fa
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-03-30 18:13:46 -0500

    Fix Error Raiz Cuadrada, Software Ecuación cuadratica

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math 
+
 print("""Bienvenido usuario, para resolver la ecuacion cuadratica, primero ingrese el
 valor del numero que acompaña a la  elevada al cuadrado, luego el que acompaña a la 
 x y por último ingrese el numero sin la x.""")
@@ -9,8 +11,8 @@ c=int(input("3."))
 d = b**2 - (4*a*c)
 
 if d > 0:
-    x1 = (-b+(d**1/2))/2*a
-    x2 = (-b-(d**1/2))/2*a
+    x1 = (-b+math.sqrt(d))/2*a
+    x2 = (-b-math.sqrt(d))/2*a
     print("X vale 0 cuando toma el valor de",x1,"y ",x2,)
 elif d < 0:
     print("No existe solución a la ecuación cuadrática dentro del dominio de los números reales.")
