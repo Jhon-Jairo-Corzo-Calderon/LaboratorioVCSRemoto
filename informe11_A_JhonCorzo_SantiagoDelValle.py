ec0814215d63a63d8677cff6888677e4e5c57a1a
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-04-18 19:48:59 -0500

    Parte A,punto b.

--- informe11_A_JhonCorzo_SantiagoDelValle.py
+++ informe11_A_JhonCorzo_SantiagoDelValle.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+#Punto a
 def generador(min,max):
     array = np.random.randint(min,max,size=48).reshape(4,12)
     return array
+
+#Punto b
+ingresos = generador(100,181)
+egresos = generador(60,131)
