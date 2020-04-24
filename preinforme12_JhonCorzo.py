a8c3731df901fafa8c094786b352e8f7d4235d34
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-04-24 12:18:39 -0500

    Parte A,Ejercicio 2.

--- preinforme12_JhonCorzo.py
+++ preinforme12_JhonCorzo.py
@@ -6,3 +6,17 @@ presion_semanal= [110.06,107.89,108.45,108.49,109.03,110.11,109.87,119.38,119.35
                   117.49,109.65,110.44,110.51,107.38,109.26,106.18,109.36,106.61,105.16,110.11,105.48,108.37,107.59,108.91,108.35,109.57,122.56,
                   124.44,125.97,121.03,121.22,122.41,122.15,124.52,123.35,125.76,121.08,122.29,105.42,110.67,107.73,105.76,107.85]
 
+#Ejercicio 2
+mayor=0
+for i in presion_semanal:
+    if i>mayor:
+        mayor=i
+
+menor=150
+for x in presion_semanal:
+    if x<menor:
+        menor=x
+
+dif=mayor-menor
+
+print("El resultado de la diferencia entre la mayor y la menor presión promedio semanal registrada es de",dif)
